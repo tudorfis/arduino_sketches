0b1e6853c5c4ad591ee52c91e0eddf01c63e7468
tudorfis <tudor.fis@gmail.com>
2021-05-15 15:06:25 +0300

    -

--- mk1_robo_arm/mk1_robo_arm.ino
+++ mk1_robo_arm/mk1_robo_arm.ino
@@ -5,17 +5,21 @@ Servo updownservo;
 Servo forwardservo;
 
 int gripservo_value = 10;
-int baseservo_value = 0;
-int updownservo_value = 0;
-int forwardservo_value = 0;
+int baseservo_value = 1;
+int updownservo_value = 1;
+int forwardservo_value = 1;
 boolean is_grip_open = false;
+
+int modeSpeed = 1;
+int maxModeSpeed = 7;
+int multiplier = 3;
 
 void setup() 
 {
   gripservo.attach( A0 );
   baseservo.attach( A1 );
-  updownservo.attach( A3 );
   forwardservo.attach( A2 );
+  updownservo.attach( A3 );
   
   Serial.begin(9600);
 }
@@ -29,26 +33,29 @@ void loop()
     
     if ( keypressed == 'U' ) {
         updownservo_value += 10;
+//        updownservo_value = updownservo_value * modeSpeed * multiplier;
         
         if ( updownservo_value > 180 ) {
           updownservo_value = 180;
         }
     } else if ( keypressed == 'D' ) {
         updownservo_value -= 10;
+//        updownservo_value = updownservo_value * modeSpeed * multiplier;
         
         if ( updownservo_value < 0 ) {
           updownservo_value = 0;
         }
-
         
     } else if ( keypressed == 'L' ) {
         baseservo_value += 10;
+//        baseservo_value = baseservo_value * modeSpeed * multiplier;
         
         if ( baseservo_value > 180 ) {
           baseservo_value = 180;
         }
     } else if ( keypressed == 'R' ) {
         baseservo_value -= 10;
+//        baseservo_value = baseservo_value * modeSpeed * multiplier;
         
         if ( baseservo_value < 0 ) {
           baseservo_value = 0;
@@ -57,12 +64,14 @@ void loop()
         
     } else if ( keypressed == 'H' ) {
         forwardservo_value += 10;
+//        forwardservo_value = forwardservo_value * modeSpeed * multiplier;
         
         if ( forwardservo_value > 180 ) {
           forwardservo_value = 180;
         }
     } else if ( keypressed == 'K' ) {
         forwardservo_value -= 10;
+//        forwardservo_value = forwardservo_value * modeSpeed * multiplier;
         
         if ( forwardservo_value < 0 ) {
           forwardservo_value = 0;
@@ -75,11 +84,24 @@ void loop()
       } else {
         is_grip_open = true;
       }
+    } else if ( keypressed == 'T' ) {
+      modeSpeed += 1;
+      
+      if ( modeSpeed > maxModeSpeed ) {
+        modeSpeed = 1;
+      }
     }
   }
 
-  Serial.println( updownservo_value );
-
+  Serial.print( "baseservo_value= " );
+  Serial.print( baseservo_value );
+  
+  Serial.print( " updownservo_value= " );
+  Serial.print(  updownservo_value );
+  
+  Serial.print( " forwardservo_value= " );
+  Serial.println( forwardservo_value );
+  
   if ( is_grip_open ) {
     gripservo_value = 50;
   } else {
@@ -88,8 +110,8 @@ void loop()
   
   gripservo.write( gripservo_value );
   baseservo.write( baseservo_value );
-//  forwardservo.write( forwardservo_value );
-//  updownservo.write( updownservo_value );
+  forwardservo.write( forwardservo_value );
+  updownservo.write( updownservo_value );
   
   delay( 50 );
 }
